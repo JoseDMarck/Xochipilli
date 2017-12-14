6aafc9821e7e114bf86d87ca2747d625891f2e0e
Deuteros <josewbmx@gmail.com>
2017-12-14 12:59:59 -0600

    Se agregaron las funcionalidades para que al entrar y salir de pantallas se haga con un fade

---
+++ js/Eventos/p0-ScreenSaver/main.js
@@ -0,0 +1,30 @@
+$( document ).ready(function() {
+   TweenMax.to(".btnIniciar", 2, {alpha:0,  repeat:-1, yoyo:true});
+
+
+
+/* --------------------------------------------------------
+/* ---- Eventos interacción de pantalla
+-------------------------------------------------------- */
+
+var ExitScreen = function () {
+    window.location.href = 'secciones/p1-presentacion/index.html';
+};
+
+var InitScreen = function () {
+    TweenMax.to("body", 2, {alpha:1});
+};
+
+InitScreen();
+
+
+
+//GO TO P1: PRESENTACIÓN
+$(".touch-bg").click(function() {
+	 TweenMax.to("body", 1, {alpha:0, onComplete: ExitScreen });
+});
+
+
+
+
+}); // onDocumetReady
