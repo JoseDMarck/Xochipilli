ce9a1540dea84171988072f9086a4961d3836598
Deuteros <josewbmx@gmail.com>
2017-12-15 12:47:31 -0600

    Se cambió el nombre de presentacion

--- js/Eventos/p01-Presentacion/main.js
+++ js/Eventos/p01-Presentacion/main.js
@@ -12,12 +12,22 @@ var ExitScreen = function () {
     window.location.href = 'secciones/p01-presentacion/index.html';
 };
 
+
+var GotoHome = function () {
+    window.location.href = '../../secciones/p02-home/index.html';
+};
+
+
+
 var closem = function () {
     TweenMax.to(".menu-General", 1, {right:"-11%", alpha:1, onComplete: closem });
 };
 
 
-
+// Bnt Continuar
+$(".btn_continuar").click(function() {
+	TweenMax.to("body", 1, {alpha:0, onComplete: GotoHome });
+});
 
 var InitScreen = function () {
     TweenMax.to("body", 3, {alpha:1});
