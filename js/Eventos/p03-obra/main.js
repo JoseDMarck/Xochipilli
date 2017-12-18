658ad97a11499a008c2e6747d93f198fd8f347da
Deuteros <josewbmx@gmail.com>
2017-12-18 13:04:18 -0600

    Se cambio la funcionalidad de continuar por la de de home en los eventos de la pantalla de Obra

--- js/Eventos/p03-obra/main.js
+++ js/Eventos/p03-obra/main.js
@@ -33,13 +33,10 @@ var opensem = function () {
 };
 
 
-// Bnt Continuar
-$(".btn_continuar").click(function() {
-	TweenMax.to("body", 1, {alpha:0, onComplete: GotoHome });
-});
+ 
 
-$(".btn_back").click(function() {
-    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoBack});
+$(".btn_home").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome});
 });
 
 
@@ -48,32 +45,15 @@ $(".btn_Obra").click(function() {
 });
 
 
+
 var InitScreen = function () {
     TweenMax.to("body", 3, {alpha:1});
-    //TweenMax.to(".menu-General", 1, {right:"0%", alpha:1, onComplete: closem });
-
-    TweenMax.to(".BloqueA",1.5, {left:"0%",     alpha:1, ease: Sine.easeOut });
-    TweenMax.to(".BloqueB",1.5, {left:"33.33%", alpha:1, ease: Sine.easeOut });
-    TweenMax.to(".BloqueC",1.5, {left:"66.66%", alpha:1, ease: Sine.easeOut, onComplete: opensem  });
 };
 
 InitScreen();
 
 
-// EVENTOS MENU ===================
-
-$(".btn_open_menu").click(function() {
-    TweenMax.to(".menu-General", 0.5, {right:"0%", alpha:1});
-    $(".btn_open_menu").hide();
-    $(".btn_close_menu").show();
-});
-
-
-$(".btn_close_menu").click(function() {
-    TweenMax.to(".menu-General", 0.5, {right:"-11%", alpha:1});
-    $(".btn_open_menu").show();
-    $(".btn_close_menu").hide();
-});
+ 
 
 
 // EVENTOS AYUDA ===================
