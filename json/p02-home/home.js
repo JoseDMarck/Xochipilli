b8e6b3d11a675b23128c2b35c0c6e5c3b0a07158
Deuteros <josewbmx@gmail.com>
2017-12-19 16:11:40 -0600

    Se agregó el json del Home

---
+++ json/p02-home/home.js
@@ -0,0 +1,50 @@
+// IDIOMA ESPAÑOL
+
+var obj_ESP = {
+    botones: 
+        [{
+            btn_continuar: "Continuar",
+            btn_glosario: "Glosario",
+            btn_referencias: "Referencias",
+            btn_creditos: "Créditos",
+            btn_info_access: "Información",
+            btn_obra: "Obra",
+            btn_interpretacion: "Interpretación",
+            btn_gabinete: "Gabinete Botánico"
+
+        }]  
+};
+
+
+// IDIOMA INGLÉS
+var obj_ENG = {
+    botones: 
+        [{
+            btn_continuar: "Continue",
+            btn_glosario: "Glossary",
+            btn_referencias: "References",
+            btn_creditos: "Credits",
+            btn_info_access: "Information",
+            btn_obra: "Work",
+            btn_interpretacion: "Interpretation",
+            btn_gabinete: "Botanical Cabinet"
+
+        }]  
+};
+
+
+// IDIOMA Nahuatlatoltica
+var obj_NAH = {
+    botones: 
+        [{
+            btn_continuar: "Toka",
+            btn_glosario: "Amoxtlajtoli",
+            btn_referencias: "Chikomostok",
+            btn_creditos: "Tlajtolkakilia",
+            btn_info_access: "Tlanonotsalistli",
+            btn_obra: "Altepekipanolistli",
+            btn_interpretacion: "Kuapatlajtolistli",
+            btn_gabinete: "Xochitlayokan"
+
+        }]  
+};
