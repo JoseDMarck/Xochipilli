59546f7c7bc7be88f1c240723449bcae4f45d31d
Deuteros <josewbmx@gmail.com>
2017-12-21 17:45:38 -0600

    Se agregaron los obj para el 3D

--- js/Eventos/p05-interpretacion/jsonEvents.js
+++ js/Eventos/p05-interpretacion/jsonEvents.js
@@ -6,7 +6,17 @@ $( document ).ready(function() {
 
 if (LanguageON == "ESP"){
 
- 
+ // TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
+
+// Textos Galerias
+document.getElementById("btn_lectura").innerHTML = obj_ESP.botones[0].btn_lectura; 
+document.getElementById("btn_atavios_sol").innerHTML = obj_ESP.botones[0].btn_atavios_sol;
+document.getElementById("btn_atavios_tierra").innerHTML = obj_ESP.botones[0].btn_atavios_tierra; 
+document.getElementById("btn_atavios_florido").innerHTML = obj_ESP.botones[0].btn_atavios_florido; 
+document.getElementById("btn_trono").innerHTML = obj_ESP.botones[0].btn_trono; 
+
+
 }
 
 
@@ -14,6 +24,15 @@ if (LanguageON == "ESP"){
  
 if (LanguageON == "ENG"){
 
+ // TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
+
+// Textos Galerias
+document.getElementById("btn_lectura").innerHTML = obj_ENG.botones[0].btn_lectura; 
+document.getElementById("btn_atavios_sol").innerHTML = obj_ENG.botones[0].btn_atavios_sol;
+document.getElementById("btn_atavios_tierra").innerHTML = obj_ENG.botones[0].btn_atavios_tierra; 
+document.getElementById("btn_atavios_florido").innerHTML = obj_ENG.botones[0].btn_atavios_florido; 
+document.getElementById("btn_trono").innerHTML = obj_ENG.botones[0].btn_trono; 
  
 
 }
@@ -22,7 +41,15 @@ if (LanguageON == "ENG"){
 // IDIOMA NAH
 if (LanguageON == "NAH"){
  
+ // TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_NAH.titulo; 
 
+// Textos Galerias
+document.getElementById("btn_lectura").innerHTML = obj_NAH.botones[0].btn_lectura; 
+document.getElementById("btn_atavios_sol").innerHTML = obj_NAH.botones[0].btn_atavios_sol;
+document.getElementById("btn_atavios_tierra").innerHTML = obj_NAH.botones[0].btn_atavios_tierra; 
+document.getElementById("btn_atavios_florido").innerHTML = obj_NAH.botones[0].btn_atavios_florido; 
+document.getElementById("btn_trono").innerHTML = obj_NAH.botones[0].btn_trono; 
 
 } // END NAH
 
