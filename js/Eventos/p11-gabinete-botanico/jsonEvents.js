493a31a960cc4502eba0b1ec73d7624247b4cf88
Deuteros <josewbmx@gmail.com>
2017-12-27 16:41:50 -0600

    Se agregaron los JS de Gabinete Botánico

---
+++ js/Eventos/p11-gabinete-botanico/jsonEvents.js
@@ -0,0 +1,134 @@
+$( document ).ready(function() {
+   
+  
+if (LanguageON == "ESP"){
+
+
+// TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_ESP.descripcion; 
+
+// Titulos para los botones
+document.getElementById("titulo_btn_1").innerHTML = obj_ESP.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_ESP.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_ESP.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_ESP.titulos_btns[0].figura_4; 
+document.getElementById("titulo_btn_5").innerHTML = obj_ESP.titulos_btns[0].figura_5; 
+document.getElementById("titulo_btn_6").innerHTML = obj_ESP.titulos_btns[0].figura_6; 
+document.getElementById("titulo_btn_7").innerHTML = obj_ESP.titulos_btns[0].figura_7; 
+document.getElementById("titulo_btn_8").innerHTML = obj_ESP.titulos_btns[0].figura_8;
+document.getElementById("titulo_btn_9").innerHTML = obj_ESP.titulos_btns[0].figura_9;
+document.getElementById("titulo_btn_10").innerHTML = obj_ESP.titulos_btns[0].figura_10; 
+document.getElementById("titulo_btn_11").innerHTML = obj_ESP.titulos_btns[0].figura_11; 
+document.getElementById("titulo_btn_12").innerHTML = obj_ESP.titulos_btns[0].figura_12; 
+
+
+
+// Para las descripciones
+document.getElementById("gal_desc-1-1").innerHTML = obj_ESP.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_ESP.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_ESP.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_ESP.desc_gal[0].descripcion_1_4; 
+document.getElementById("gal_desc-1-5").innerHTML = obj_ESP.desc_gal[0].descripcion_1_5; 
+document.getElementById("gal_desc-1-6").innerHTML = obj_ESP.desc_gal[0].descripcion_1_6; 
+document.getElementById("gal_desc-1-7").innerHTML = obj_ESP.desc_gal[0].descripcion_1_7; 
+document.getElementById("gal_desc-1-8").innerHTML = obj_ESP.desc_gal[0].descripcion_1_8; 
+document.getElementById("gal_desc-1-9").innerHTML = obj_ESP.desc_gal[0].descripcion_1_9; 
+document.getElementById("gal_desc-1-10").innerHTML = obj_ESP.desc_gal[0].descripcion_1_10; 
+document.getElementById("gal_desc-1-11").innerHTML = obj_ESP.desc_gal[0].descripcion_1_11; 
+document.getElementById("gal_desc-1-12").innerHTML = obj_ESP.desc_gal[0].descripcion_1_12; 
+
+
+ 
+}
+
+
+ 
+if (LanguageON == "ENG"){
+
+// TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_ENG.descripcion; 
+
+// Titulos para los botones
+document.getElementById("titulo_btn_1").innerHTML = obj_ENG.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_ENG.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_ENG.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_ENG.titulos_btns[0].figura_4; 
+document.getElementById("titulo_btn_5").innerHTML = obj_ENG.titulos_btns[0].figura_5; 
+document.getElementById("titulo_btn_6").innerHTML = obj_ENG.titulos_btns[0].figura_6; 
+document.getElementById("titulo_btn_7").innerHTML = obj_ENG.titulos_btns[0].figura_7; 
+document.getElementById("titulo_btn_8").innerHTML = obj_ENG.titulos_btns[0].figura_8;
+document.getElementById("titulo_btn_9").innerHTML = obj_ENG.titulos_btns[0].figura_9;
+document.getElementById("titulo_btn_10").innerHTML = obj_ENG.titulos_btns[0].figura_10; 
+document.getElementById("titulo_btn_11").innerHTML = obj_ENG.titulos_btns[0].figura_11; 
+document.getElementById("titulo_btn_12").innerHTML = obj_ENG.titulos_btns[0].figura_12;  
+
+// Para las descripciones
+document.getElementById("gal_desc-1-1").innerHTML = obj_ENG.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_ENG.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_ENG.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_ENG.desc_gal[0].descripcion_1_4; 
+document.getElementById("gal_desc-1-5").innerHTML = obj_ENG.desc_gal[0].descripcion_1_5; 
+document.getElementById("gal_desc-1-6").innerHTML = obj_ENG.desc_gal[0].descripcion_1_6; 
+document.getElementById("gal_desc-1-7").innerHTML = obj_ENG.desc_gal[0].descripcion_1_7; 
+document.getElementById("gal_desc-1-8").innerHTML = obj_ENG.desc_gal[0].descripcion_1_8; 
+document.getElementById("gal_desc-1-9").innerHTML = obj_ENG.desc_gal[0].descripcion_1_9; 
+document.getElementById("gal_desc-1-10").innerHTML = obj_ENG.desc_gal[0].descripcion_1_10; 
+document.getElementById("gal_desc-1-11").innerHTML = obj_ENG.desc_gal[0].descripcion_1_11; 
+document.getElementById("gal_desc-1-12").innerHTML = obj_ENG.desc_gal[0].descripcion_1_12; 
+
+
+}
+
+if (LanguageON == "NAH"){
+
+    // TITULO OBRA
+document.getElementById("titulo").innerHTML = obj_NAH.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_NAH.descripcion; 
+
+// Titulos para los botones
+document.getElementById("titulo_btn_1").innerHTML = obj_NAH.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_NAH.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_NAH.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_NAH.titulos_btns[0].figura_4; 
+document.getElementById("titulo_btn_5").innerHTML = obj_NAH.titulos_btns[0].figura_5; 
+document.getElementById("titulo_btn_6").innerHTML = obj_NAH.titulos_btns[0].figura_6; 
+document.getElementById("titulo_btn_7").innerHTML = obj_NAH.titulos_btns[0].figura_7; 
+document.getElementById("titulo_btn_8").innerHTML = obj_NAH.titulos_btns[0].figura_8;
+document.getElementById("titulo_btn_8").innerHTML = obj_NAH.titulos_btns[0].figura_9;
+document.getElementById("titulo_btn_10").innerHTML = obj_NAH.titulos_btns[0].figura_10; 
+document.getElementById("titulo_btn_11").innerHTML = obj_NAH.titulos_btns[0].figura_11; 
+document.getElementById("titulo_btn_12").innerHTML = obj_NAH.titulos_btns[0].figura_12; 
+
+
+// Para las descripciones
+
+document.getElementById("gal_desc-1-1").innerHTML = obj_NAH.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_NAH.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_NAH.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_NAH.desc_gal[0].descripcion_1_4; 
+document.getElementById("gal_desc-1-5").innerHTML = obj_NAH.desc_gal[0].descripcion_1_5; 
+document.getElementById("gal_desc-1-6").innerHTML = obj_NAH.desc_gal[0].descripcion_1_6; 
+document.getElementById("gal_desc-1-7").innerHTML = obj_NAH.desc_gal[0].descripcion_1_7; 
+document.getElementById("gal_desc-1-8").innerHTML = obj_NAH.desc_gal[0].descripcion_1_8; 
+document.getElementById("gal_desc-1-9").innerHTML = obj_NAH.desc_gal[0].descripcion_1_9; 
+document.getElementById("gal_desc-1-10").innerHTML = obj_NAH.desc_gal[0].descripcion_1_10; 
+document.getElementById("gal_desc-1-11").innerHTML = obj_NAH.desc_gal[0].descripcion_1_11; 
+document.getElementById("gal_desc-1-12").innerHTML = obj_NAH.desc_gal[0].descripcion_1_12; 
+ 
+ 
+
+
+} // END NAH
+
+
+TweenMax.to(".btn_hotspots-LG", 2, {alpha:0.2,  repeat:-1, yoyo:true});
+
+}); // =============== onDocumetReady
