b0214c2dc514ac424457a9cd58ca1ac3b1c4aa4f
Deuteros <josewbmx@gmail.com>
2017-12-28 11:26:29 -0600

    Se agregó la funcionalidad para mostrar las infos de forma de slider

--- js/Eventos/p01-Presentacion/main.js
+++ js/Eventos/p01-Presentacion/main.js
@@ -82,6 +82,110 @@ $(".btn_close_menu").click(function() {
 });
 
 
+// EVENTOS GALLERIA ===================
+
+var num_total = 4;
+var num_init;
+var num_gal;
+
+
+$(".btn_close").click(function() {
+    $(".PopUpSlider").fadeOut();
+});
+
+$(".open_popUp").click(function() {
+  
+  valor_hp = $(this).attr('class').split(' ')[1];
+    console.log(valor_hp);
+
+    if( valor_hp == "btn_L-1-1" ){
+        num_gal = 1;
+        num_init = 1;
+    }
+
+    if( valor_hp == "btn_L-1-2" ){
+        num_gal = 1;
+        num_init = 2;
+    }
+
+    if( valor_hp == "btn_L-1-3" ){
+        num_gal = 1;
+        num_init = 3;
+    }
+
+    if( valor_hp == "btn_L-1-4" ){
+        num_gal = 1;
+        num_init = 4;
+    }
+
+    // para mostrar el btn de prev
+    if (num_init < 2 ){
+        $(".btn_prev").hide();
+    }
+    else{
+        $(".btn_prev").show();
+    }
+
+    // para mostrar el btn de next
+    if (num_init < 4  ){
+        $(".btn_next").show();
+    }
+    else{
+         $(".btn_next").hide();
+    }
+
+      $(".Textos").hide();
+      $(".PopUpSlider").fadeIn();
+      $(".seccion-m-"+num_init).fadeIn();
+
+});
+
+
+$(".btn_next").click(function() {
+     if (num_init == 3){
+        $(".btn_next").hide();
+     }
+
+    if (num_init > 0){
+        $(".btn_prev").show();
+     }
+
+
+    if (num_init < num_total){
+        $(".gal_img").hide();
+        $(".gal_desc_LG").hide();
+
+        num_init = num_init + 1;
+        console.log("num_init", num_init);
+        $(".Textos").hide();
+        $(".seccion-m-"+num_init).fadeIn();
+
+         
+    }
+});
+
+$(".btn_prev").click(function() {
+
+    if (num_init == 2){
+        $(".btn_prev").hide();
+     }
+
+    if (num_init > 1){
+        $(".gal_img").hide();
+        $(".gal_desc_LG").hide();
+
+        num_init = num_init - 1;
+        console.log("num_init", num_init);
+        $(".Textos").hide();
+        $(".seccion-m-"+num_init).fadeIn();
+
+        $(".btn_next").show();
+    }
+     
+
+});
+
+
 // EVENTOS SELECT IDIOMA ===================
 
 $(".select_ESP").click(function() {
