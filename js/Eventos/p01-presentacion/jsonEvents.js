30d870a20d3d67363f7b79e0a52f7db4582e1758
Deuteros <josewbmx@gmail.com>
2017-12-28 11:25:09 -0600

    Se agregaron los innerHTML para pintar el contenido del json

--- js/Eventos/p01-presentacion/jsonEvents.js
+++ js/Eventos/p01-presentacion/jsonEvents.js
@@ -26,6 +26,18 @@ if (LanguageON == "ESP"){
 	 
 	//btn_info_access 
 	document.getElementById("btn_info_access").innerHTML = obj_ESP.botones[0].btn_info_access; 
+
+	//btn_info_access 
+	document.getElementById("titulo_glosario").innerHTML = obj_ESP.titulo_glosario;
+	document.getElementById("descripcion_glosario").innerHTML = obj_ESP.descripcion_glosario;
+	document.getElementById("titulo_referencias").innerHTML = obj_ESP.titulo_referencias;
+	document.getElementById("descripcion_referencias").innerHTML = obj_ESP.descripcion_referencias;
+	document.getElementById("titulo_creditos").innerHTML = obj_ESP.titulo_creditos;
+	document.getElementById("descripcion_creditos").innerHTML = obj_ESP.descripcion_creditos;
+	document.getElementById("titulo_informacion").innerHTML = obj_ESP.titulo_informacion;
+	document.getElementById("descripcion_informacion").innerHTML = obj_ESP.descripcion_informacion;
+
+	 
 	 
 } // END ESP
  
@@ -53,6 +65,18 @@ if (LanguageON == "ENG"){
 	 
 	//btn_info_access 
 	document.getElementById("btn_info_access").innerHTML = obj_ENG.botones[0].btn_info_access;
+
+
+	//btn_info_access 
+	document.getElementById("titulo_glosario").innerHTML = obj_ENG.titulo_glosario;
+	document.getElementById("descripcion_glosario").innerHTML = obj_ENG.descripcion_glosario;
+	document.getElementById("titulo_referencias").innerHTML = obj_ENG.titulo_referencias;
+	document.getElementById("descripcion_referencias").innerHTML = obj_ENG.descripcion_referencias;
+	document.getElementById("titulo_creditos").innerHTML = obj_ENG.titulo_creditos;
+	document.getElementById("descripcion_creditos").innerHTML = obj_ENG.descripcion_creditos;
+	document.getElementById("titulo_informacion").innerHTML = obj_ENG.titulo_informacion;
+	document.getElementById("descripcion_informacion").innerHTML = obj_ENG.descripcion_informacion;
+
 
 
 } // END ENG
@@ -84,6 +108,17 @@ if (LanguageON == "NAH"){
 	 
 	//btn_info_access 
 	document.getElementById("btn_info_access").innerHTML = obj_NAH.botones[0].btn_info_access;
+
+
+	//btn_info_access 
+	document.getElementById("titulo_glosario").innerHTML = obj_NAH.titulo_glosario;
+	document.getElementById("descripcion_glosario").innerHTML = obj_NAH.descripcion_glosario;
+	document.getElementById("titulo_referencias").innerHTML = obj_NAH.titulo_referencias;
+	document.getElementById("descripcion_referencias").innerHTML = obj_NAH.descripcion_referencias;
+	document.getElementById("titulo_creditos").innerHTML = obj_NAH.titulo_creditos;
+	document.getElementById("descripcion_creditos").innerHTML = obj_NAH.descripcion_creditos;
+	document.getElementById("titulo_informacion").innerHTML = obj_NAH.titulo_informacion;
+	document.getElementById("descripcion_informacion").innerHTML = obj_NAH.descripcion_informacion;
 
 
 } // END NAH
