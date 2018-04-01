52400f6d375aaddd00705e510005db196a4fa977
joseDeuteros <josewbmx@gmail.com>
2018-04-01 00:24:31 -0600

    Se quitaron los metodos que no necesitaba para la pantalla del home

--- js/Eventos/p02-home/jsonEvents.js
+++ js/Eventos/p02-home/jsonEvents.js
@@ -6,37 +6,11 @@ $( document ).ready(function() {
 
 if (LanguageON == "ESP"){
 
-
-	//btn_glosario
-	document.getElementById("btn_glosario").innerHTML = obj_ESP.botones[0].btn_glosario; 
-	
-	//btn_referencias
-	document.getElementById("btn_referencias").innerHTML = obj_ESP.botones[0].btn_referencias; 
-	 
-	//btn_creditos
-	document.getElementById("btn_creditos").innerHTML = obj_ESP.botones[0].btn_creditos; 
-	 
 	//btn_info_access 
-	document.getElementById("btn_info_access").innerHTML = obj_ESP.botones[0].btn_info_access; 
-
-	//btn_info_access 
-	document.getElementById("btn_obra").innerHTML = obj_ESP.botones[0].btn_obra;
-
-	//btn_info_access 
-		document.getElementById("btn_interpretacion").innerHTML = obj_ESP.botones[0].btn_interpretacion;
-	
-	//btn_info_access 
-		document.getElementById("btn_gabinete").innerHTML = obj_ESP.botones[0].btn_gabinete;
-
-	//btn_info_access 
-	document.getElementById("titulo_glosario").innerHTML = obj_ESP.titulo_glosario;
 	document.getElementById("descripcion_glosario").innerHTML = obj_ESP.descripcion_glosario;
-	document.getElementById("titulo_referencias").innerHTML = obj_ESP.titulo_referencias;
 	document.getElementById("descripcion_referencias").innerHTML = obj_ESP.descripcion_referencias;
-	document.getElementById("titulo_creditos").innerHTML = obj_ESP.titulo_creditos;
 	document.getElementById("descripcion_creditos").innerHTML = obj_ESP.descripcion_creditos;
-	document.getElementById("titulo_informacion").innerHTML = obj_ESP.titulo_informacion;
-	document.getElementById("descripcion_informacion").innerHTML = obj_ESP.descripcion_informacion;
+	document.getElementById("descripcion_conservacion").innerHTML = obj_ESP.descripcion_conservacion;
 	 
 } // END ESP
  
@@ -45,36 +19,12 @@ if (LanguageON == "ESP"){
 // IDIOMA INGLÉS 
 if (LanguageON == "ENG"){
 	 
-	//btn_glosario
-	document.getElementById("btn_glosario").innerHTML = obj_ENG.botones[0].btn_glosario; 
-	
-	//btn_referencias
-	document.getElementById("btn_referencias").innerHTML = obj_ENG.botones[0].btn_referencias; 
-	 
-	//btn_creditos
-	document.getElementById("btn_creditos").innerHTML = obj_ENG.botones[0].btn_creditos; 
-	 
-	//btn_info_access 
-	document.getElementById("btn_info_access").innerHTML = obj_ENG.botones[0].btn_info_access;
- 
-	//btn_info_access 
-	document.getElementById("btn_obra").innerHTML = obj_ENG.botones[0].btn_obra;
 
 	//btn_info_access 
-		document.getElementById("btn_interpretacion").innerHTML = obj_ENG.botones[0].btn_interpretacion;
-	
-	//btn_info_access 
-		document.getElementById("btn_gabinete").innerHTML = obj_ENG.botones[0].btn_gabinete;
-
-	//btn_info_access 
-	document.getElementById("titulo_glosario").innerHTML = obj_ENG.titulo_glosario;
 	document.getElementById("descripcion_glosario").innerHTML = obj_ENG.descripcion_glosario;
-	document.getElementById("titulo_referencias").innerHTML = obj_ENG.titulo_referencias;
 	document.getElementById("descripcion_referencias").innerHTML = obj_ENG.descripcion_referencias;
-	document.getElementById("titulo_creditos").innerHTML = obj_ENG.titulo_creditos;
 	document.getElementById("descripcion_creditos").innerHTML = obj_ENG.descripcion_creditos;
-	document.getElementById("titulo_informacion").innerHTML = obj_ENG.titulo_informacion;
-	document.getElementById("descripcion_informacion").innerHTML = obj_ENG.descripcion_informacion;
+	document.getElementById("descripcion_conservacion").innerHTML = obj_ENG.descripcion_conservacion;
 
 } // END ENG
 
@@ -85,37 +35,12 @@ if (LanguageON == "ENG"){
 // IDIOMA NAH
 if (LanguageON == "NAH"){
 
-	 
-	//btn_glosario
-	document.getElementById("btn_glosario").innerHTML = obj_NAH.botones[0].btn_glosario; 
-	
-	//btn_referencias
-	document.getElementById("btn_referencias").innerHTML = obj_NAH.botones[0].btn_referencias; 
-	 
-	//btn_creditos
-	document.getElementById("btn_creditos").innerHTML = obj_NAH.botones[0].btn_creditos; 
-	 
 	//btn_info_access 
-	document.getElementById("btn_info_access").innerHTML = obj_NAH.botones[0].btn_info_access;
 
-	//btn_info_access 
-	document.getElementById("btn_obra").innerHTML = obj_NAH.botones[0].btn_obra;
-
-	//btn_info_access 
-	document.getElementById("btn_interpretacion").innerHTML = obj_NAH.botones[0].btn_interpretacion;
-	
-	//btn_info_access 
-	document.getElementById("btn_gabinete").innerHTML = obj_NAH.botones[0].btn_gabinete;
-
-	//btn_info_access 
-	document.getElementById("titulo_glosario").innerHTML = obj_NAH.titulo_glosario;
 	document.getElementById("descripcion_glosario").innerHTML = obj_NAH.descripcion_glosario;
-	document.getElementById("titulo_referencias").innerHTML = obj_NAH.titulo_referencias;
 	document.getElementById("descripcion_referencias").innerHTML = obj_NAH.descripcion_referencias;
-	document.getElementById("titulo_creditos").innerHTML = obj_NAH.titulo_creditos;
 	document.getElementById("descripcion_creditos").innerHTML = obj_NAH.descripcion_creditos;
-	document.getElementById("titulo_informacion").innerHTML = obj_NAH.titulo_informacion;
-	document.getElementById("descripcion_informacion").innerHTML = obj_NAH.descripcion_informacion;
+	document.getElementById("descripcion_conservacion").innerHTML = obj_NAH.descripcion_conservacion;
 
 } // END NAH
 
