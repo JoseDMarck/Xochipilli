82574bde8c4bca37f206d152c86cb1c0fbb255d9
joseDeuteros <josewbmx@gmail.com>
2018-04-01 17:43:36 -0500

    se cambiaron agregaron las funciones para los menus

--- js/Eventos/p03-obra/main.js
+++ js/Eventos/p03-obra/main.js
@@ -6,16 +6,39 @@ $( document ).ready(function() {
 -------------------------------------------------------- */
 
 HomeURL = '../../index.html';
+statusMenu = 0;
 
 // IDIOMA DETECCIÓN
 LanguageON = window.location.hash.substring(1)
 
 if (LanguageON != "" ){
-    console.log("Idioma: ", LanguageON);
-} else {
-   LanguageON = "ESP"; 
-   console.log("Idioma: Default");
-}
+    
+        if( LanguageON == "ESP" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_es_General").addClass("btn_idioma_es_on_General");
+    
+        } 
+        
+        if( LanguageON == "ENG" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_en_General").addClass("btn_idioma_en_on_General");
+            
+          
+        }
+    
+        if( LanguageON == "NAH" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_na_General").addClass("btn_idioma_na_on_General");
+            
+           
+        } 
+    
+       
+    } else {
+       LanguageON = "ESP"; 
+       console.log("Idioma: Default");
+    }
+    
 
 var GotoObra = function () {
     window.location.href = '../../secciones/p03-obra/index.html';
@@ -44,10 +67,32 @@ var opensem = function () {
 };
 
 
+
+var Change_idioma_ESP = function () {
+    location.reload();
+    window.location.href = '../../secciones/p03-obra/index.html'+'#'+'ESP';
+};
+
+
+var Change_idioma_ENG = function () {
+    location.reload();
+    window.location.href = '../../secciones/p03-obra/index.html'+'#'+'ENG';
+};
+
+var Change_idioma_NAH = function () {
+    location.reload();
+    window.location.href = '../../secciones/p03-obra/index.html'+'#'+'NAH';
+};
+
+
  
 
 $(".btn_home").click(function() {
     TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome});
+});
+
+$(".btn_back").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome });
 });
 
 
@@ -57,86 +102,121 @@ $(".btn_Obra").click(function() {
 
 
 
+
+
+
 $(".btn_otrasPres").click(function() {
     TweenMax.to("body", 1, {alpha:0, onComplete:  GotoRepresentaciones});
 });
 
+
+$(".btn_up").click(function() {
+    if(statusMenu == 0){
+        TweenMax.to(".menuBarraGeneral", 1, {y:-180, onComplete: show = function(){
+           $(".btn_back").fadeIn(); 
+           TweenMax.to(".btn_up", 0.5, {rotation:180, y:32,  ease: Circ.easeOut})
+        }});
+        
+        statusMenu = 1;
+    }else{
+        $(".btn_back").hide(); 
+        TweenMax.to(".menuBarraGeneral", 1, {y:0, onComplete: returPos = function(){
+            TweenMax.to(".btn_up", 0.5, {rotation:0, y:0,  ease: Circ.easeOut})
+        }});
+        
+        
+        statusMenu = 0;
+
+    }
+});
 
 
 
 
 var InitScreen = function () {
     TweenMax.to("body", 3, {alpha:1});
+
+     // Para efecto de ayuda
+     TweenMax.to(".ayuda-image", 0.5, {alpha: 1, width:1920, height:1080, alpha: 1, x:0, y:0, rotation:360,  ease: Circ.easeOut, onComplete: 
+        close = function(){
+
+            CloseAyudaOnInitEvent = setTimeout(function(){ 
+                TweenMax.to(".ayuda-image", 0.3, {alpha: 1, width:0, height:0, alpha: 0, x:275, y: -20, rotation:180, ease: Circ.easeOut})
+            }, 5000);
+            
+        }   
+    })
+
+
 };
 
 InitScreen();
 
 
- 
 
 
 // EVENTOS AYUDA ===================
-
-$(".btn_ayuda").click(function() {
-    $(".ayuda-image").fadeIn();   
+$(".btn_ayuda_General").click(function() {
+    TweenMax.to(".ayuda-image", 0.5, { alpha: 1, width:1920, height:1080, x:0, y:0, rotation:360, ease: Circ.easeOut});    
 });
 
 $(".ayuda-image").click(function() {
-    $(".ayuda-image").fadeOut();   
+    TweenMax.to(".ayuda-image", 0.3, {alpha: 1, width:0, height:0, x:275, y: -20, rotation:180,   ease: Circ.easeOut});    
+    clearTimeout(CloseAyudaOnInitEvent); // Limpiamos el timer de 5 segundos
 });
+
 
 
 
 // EVENTOS GALLERIA ===================
 
-var num_total = 3;
-var num_init = 1;
+var num_total_G = 3;
+var num_init_G = 1;
 
 
-$(".next_btn").click(function() {
+$(".next_btn_obra").click(function() {
 
-     if (num_init == 2){
-        $(".next_btn").hide();
+     if (num_init_G == 2){
+        $(".next_btn_obra").hide();
      }
 
-    if (num_init > 0){
-        $(".prev_btn").show();
+    if (num_init_G > 0){
+        $(".prev_btn_obra").show();
      }
 
 
-    if (num_init < num_total){
-        $(".gal_img").hide();
-        $(".gal_txt").hide();
+    if (num_init_G < num_total_G){
+        $(".gal_img_G").hide();
+        $(".gal_txt_G").hide();
 
-        num_init = num_init + 1;
-        console.log("num_init", num_init);
-        $(".obra_img_"+num_init).fadeIn();
-        $(".gal_txt_"+num_init).fadeIn();
+        num_init_G = num_init_G + 1;
+        console.log("num_init_G_", num_init_G);
+        $(".gal_img_G_"+num_init_G).fadeIn();
+        $(".gal_txt_G_"+num_init_G).fadeIn();
     }
      
 
 });
 
 
-$(".prev_btn").click(function() {
+$(".prev_btn_obra").click(function() {
 
-    if (num_init == 2){
-        $(".prev_btn").hide();
+    if (num_init_G == 2){
+        $(".prev_btn_obra").hide();
      }
 
-    if (num_init > 1){
-        $(".gal_img").hide();
-        $(".gal_txt").hide();
+    if (num_init_G > 1){
+        $(".gal_img_G").hide();
+        $(".gal_txt_G").hide();
 
-        num_init = num_init - 1;
-        console.log("num_init", num_init);
-        $(".obra_img_"+num_init).fadeIn();
-        $(".gal_txt_"+num_init).fadeIn();
+        num_init_G = num_init_G - 1;
+        console.log("num_init_G_", num_init_G);
+        $(".gal_img_G_"+num_init_G).fadeIn();
+        $(".gal_txt_G_"+num_init_G).fadeIn();
 
-        $(".next_btn").show();
+        $(".next_btn_obra").show();
     }
      
-
 });
 
 
@@ -145,11 +225,40 @@ $(".prev_btn").click(function() {
 $(function () {
     var viewer = ImageViewer({zoomValue:160});
     $('.full_btn').click(function () {
-        var imgSrc = "../../images/p03-obra/full_img"+num_init+".jpg",
+        var imgSrc = "../../images/p03-obra/full_img"+num_init_G+".jpg",
         highResolutionImage = $(this).data('high-res-img');
         viewer.show(imgSrc, highResolutionImage);
     });
 }); 
+
+
+
+
+// EVENTOS SELECT IDIOMA ===================
+
+$(".btn_idioma_es_General").click(function(event) {
+    TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ESP });
+    LanguageON = null;
+    event.stopPropagation();
+});
+
+$(".btn_idioma_en_General").click(function(event) {
+    TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ENG });
+    LanguageON = null;
+    event.stopPropagation();
+});
+
+$(".btn_idioma_na_General").click(function(event) {
+    TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_NAH});
+    LanguageON = null;
+    event.stopPropagation();
+});
+
+
+
+
+
+
 
 
 
