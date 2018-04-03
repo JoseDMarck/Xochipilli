23c1ae0e1f362a520f44a50bee974f353f4ae96a
joseDeuteros <josewbmx@gmail.com>
2018-04-03 16:07:57 -0500

    Se agregaron los js para los menus

--- js/Eventos/p07-senior-flores/main.js
+++ js/Eventos/p07-senior-flores/main.js
@@ -10,12 +10,71 @@ HomeURL = '../../index.html';
 // IDIOMA DETECCIÓN
 LanguageON = window.location.hash.substring(1)
 
+
+
 if (LanguageON != "" ){
-    console.log("Idioma: ", LanguageON);
-} else {
-   LanguageON = "ESP"; 
-   console.log("Idioma: Default");
-}
+    
+        if( LanguageON == "ESP" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_es_General").addClass("btn_idioma_es_on_General");
+    
+        } 
+        
+        if( LanguageON == "ENG" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_en_General").addClass("btn_idioma_en_on_General");
+            
+          
+        }
+    
+        if( LanguageON == "NAH" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_na_General").addClass("btn_idioma_na_on_General");
+        } 
+    
+       
+    } else {
+       LanguageON = "ESP"; 
+       console.log("Idioma: Default");
+    }
+
+    var Change_idioma_ESP = function () {
+        location.reload();
+        window.location.href = '../../secciones/p07-senior-flores/index.html'+'#'+'ESP';
+    };
+    
+    
+    var Change_idioma_ENG = function () {
+        location.reload();
+        window.location.href = '../../secciones/p07-senior-flores/index.html'+'#'+'ENG';
+    };
+    
+    var Change_idioma_NAH = function () {
+        location.reload();
+        window.location.href = '../../secciones/p07-senior-flores/index.html'+'#'+'NAH';
+    };
+    
+
+    // EVENTOS SELECT IDIOMA ===================
+
+    $(".btn_idioma_es_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ESP });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_en_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ENG });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_na_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_NAH});
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
 
 var GotoObra = function () {
     window.location.href = '../../secciones/p03-obra/index.html';
@@ -49,6 +108,12 @@ $(".btn_back").click(function() {
 });
 
 
+$(".btn_inicio_General").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome});
+});
+
+
+
 $(".btn_Obra").click(function() {
     TweenMax.to("body", 1, {alpha:0, onComplete:  GotoObra });
 });
@@ -57,25 +122,39 @@ $(".btn_Obra").click(function() {
 
 var InitScreen = function () {
     TweenMax.to("body", 3, {alpha:1});
+
+     // Para efecto de ayuda
+     TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:1920, height:1080, alpha: 1, x:0, y:0, rotation:360,  ease: Circ.easeOut, onComplete: 
+        close = function(){
+
+            CloseAyudaOnInitEvent = setTimeout(function(){ 
+                TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:0, height:0, alpha: 0, x:275, y: -20, rotation:180, ease: Circ.easeOut})
+            }, 5000);
+            
+        }   
+    })
+
+
 };
+
 
 InitScreen();
 
 
- 
-
-
 // EVENTOS AYUDA ===================
-
-$(".btn_ayuda").click(function() {
-    $(".ayuda-image").fadeIn();   
+$(".btn_ayuda_General").click(function() {
+    TweenMax.to(".ayuda-image", 0.8, { alpha: 1, width:1920, height:1080, x:0, y:0, rotation:360, ease: Circ.easeOut});    
 });
 
 $(".ayuda-image").click(function() {
-    $(".ayuda-image").fadeOut();   
+    TweenMax.to(".ayuda-image", 0.8  , {alpha: 1, width:0, height:0, x:275, y: -20, rotation:180,   ease: Circ.easeOut});    
+    clearTimeout(CloseAyudaOnInitEvent); // Limpiamos el timer de 5 segundos
 });
 
 
+ 
+
+ 
 
 // EVENTOS GALLERIA ===================
 
