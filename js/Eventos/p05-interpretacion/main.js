0aab6d564d1e5fd323afa1436358cb3664d9d92e
joseDeuteros <josewbmx@gmail.com>
2018-04-03 09:58:28 -0500

    Se agregaron los eventos al menu

--- js/Eventos/p05-interpretacion/main.js
+++ js/Eventos/p05-interpretacion/main.js
@@ -11,11 +11,70 @@ HomeURL = '../../index.html';
 LanguageON = window.location.hash.substring(1)
 
 if (LanguageON != "" ){
-    console.log("Idioma: ", LanguageON);
-} else {
-   LanguageON = "ESP"; 
-   console.log("Idioma: Default");
-}
+    
+        if( LanguageON == "ESP" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_es_General").addClass("btn_idioma_es_on_General");
+    
+        } 
+        
+        if( LanguageON == "ENG" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_en_General").addClass("btn_idioma_en_on_General");
+            
+          
+        }
+    
+        if( LanguageON == "NAH" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_na_General").addClass("btn_idioma_na_on_General");
+        } 
+    
+       
+    } else {
+       LanguageON = "ESP"; 
+       console.log("Idioma: Default");
+    }
+
+    var Change_idioma_ESP = function () {
+        location.reload();
+        window.location.href = '../../secciones/p05-interpretacion/index.html'+'#'+'ESP';
+    };
+    
+    
+    var Change_idioma_ENG = function () {
+        location.reload();
+        window.location.href = '../../secciones/p05-interpretacion/index.html'+'#'+'ENG';
+    };
+    
+    var Change_idioma_NAH = function () {
+        location.reload();
+        window.location.href = '../../secciones/p05-interpretacion/index.html'+'#'+'NAH';
+    };
+    
+
+    // EVENTOS SELECT IDIOMA ===================
+
+    $(".btn_idioma_es_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ESP });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_en_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ENG });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_na_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_NAH});
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+
+
 
 var GotoObra = function () {
     window.location.href = '../../secciones/p03-obra/index.html';
@@ -33,6 +92,12 @@ var GotoBack = function () {
 var GotoLectura = function () {
     window.location.href = '../../secciones/p06-lectura-general/index.html' +'#'+LanguageON;
 };
+
+
+var GotoSeniorFlores = function(){
+    window.location.href = '../../secciones/p07-senior-flores/index.html' +'#'+LanguageON;
+}
+
 
 var GotoAtavios_sol = function () {
     window.location.href = '../../secciones/p07-atavios-de-sol/index.html' +'#'+LanguageON;
@@ -54,6 +119,9 @@ var GotoTrono = function () {
 
 
 
+
+
+
 var closem = function () {
     TweenMax.to(".menu-General", 1, {right:"-11%", alpha:1 });
 };
@@ -67,7 +135,7 @@ var opensem = function () {
  
 
 $(".btn_back").click(function() {
-    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoBack});
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome});
 });
 
 $(".btn_home").click(function() {
@@ -83,6 +151,12 @@ $(".btn_Obra").click(function() {
 $(".btn_lectura").click(function() {
     TweenMax.to("body", 1, {alpha:0, onComplete:  GotoLectura });
 });
+
+
+$(".btn_senior_flores").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoSeniorFlores });
+});
+
 
 $(".btn_atavios_tierra").click(function() {
     TweenMax.to("body", 1, {alpha:0, onComplete:  GotoAtavios_tierra });
@@ -100,31 +174,35 @@ $(".btn_trono").click(function() {
 
 
 
-
-
-
-
 var InitScreen = function () {
     TweenMax.to("body", 3, {alpha:1});
+
+     // Para efecto de ayuda
+     TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:1920, height:1080, alpha: 1, x:0, y:0, rotation:360,  ease: Circ.easeOut, onComplete: 
+        close = function(){
+
+            CloseAyudaOnInitEvent = setTimeout(function(){ 
+                TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:0, height:0, alpha: 0, x:275, y: -20, rotation:180, ease: Circ.easeOut})
+            }, 5000);
+            
+        }   
+    })
+
 };
 
 InitScreen();
 
-
- 
-
-
 // EVENTOS AYUDA ===================
-
-
-
-$(".btn_ayuda").click(function() {
-    $(".ayuda-image").fadeIn();   
+$(".btn_ayuda_General").click(function() {
+    TweenMax.to(".ayuda-image", 0.8, { alpha: 1, width:1920, height:1080, x:0, y:0, rotation:360, ease: Circ.easeOut});    
 });
 
 $(".ayuda-image").click(function() {
-    $(".ayuda-image").fadeOut();   
+    TweenMax.to(".ayuda-image", 0.8  , {alpha: 1, width:0, height:0, x:275, y: -20, rotation:180,   ease: Circ.easeOut});    
+    clearTimeout(CloseAyudaOnInitEvent); // Limpiamos el timer de 5 segundos
 });
+
+
 
 
 
