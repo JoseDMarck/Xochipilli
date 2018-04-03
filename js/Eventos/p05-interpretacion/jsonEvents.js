b4794f7153f7e9433f58c08f3ded7bfda65fcfa0
joseDeuteros <josewbmx@gmail.com>
2018-04-03 09:58:05 -0500

    Se comentaron las lineas para poner el codigo nuevo

--- js/Eventos/p05-interpretacion/jsonEvents.js
+++ js/Eventos/p05-interpretacion/jsonEvents.js
@@ -7,14 +7,14 @@ $( document ).ready(function() {
 if (LanguageON == "ESP"){
 
  // TITULO OBRA
-document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
+//document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
 
 // Textos Galerias
-document.getElementById("btn_lectura").innerHTML = obj_ESP.botones[0].btn_lectura; 
+/*document.getElementById("btn_lectura").innerHTML = obj_ESP.botones[0].btn_lectura; 
 document.getElementById("btn_atavios_sol").innerHTML = obj_ESP.botones[0].btn_atavios_sol;
 document.getElementById("btn_atavios_tierra").innerHTML = obj_ESP.botones[0].btn_atavios_tierra; 
 document.getElementById("btn_atavios_florido").innerHTML = obj_ESP.botones[0].btn_atavios_florido; 
-document.getElementById("btn_trono").innerHTML = obj_ESP.botones[0].btn_trono; 
+document.getElementById("btn_trono").innerHTML = obj_ESP.botones[0].btn_trono; */
 
 
 }
@@ -25,14 +25,14 @@ document.getElementById("btn_trono").innerHTML = obj_ESP.botones[0].btn_trono;
 if (LanguageON == "ENG"){
 
  // TITULO OBRA
-document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
+//document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
 
 // Textos Galerias
-document.getElementById("btn_lectura").innerHTML = obj_ENG.botones[0].btn_lectura; 
+/*document.getElementById("btn_lectura").innerHTML = obj_ENG.botones[0].btn_lectura; 
 document.getElementById("btn_atavios_sol").innerHTML = obj_ENG.botones[0].btn_atavios_sol;
 document.getElementById("btn_atavios_tierra").innerHTML = obj_ENG.botones[0].btn_atavios_tierra; 
 document.getElementById("btn_atavios_florido").innerHTML = obj_ENG.botones[0].btn_atavios_florido; 
-document.getElementById("btn_trono").innerHTML = obj_ENG.botones[0].btn_trono; 
+document.getElementById("btn_trono").innerHTML = obj_ENG.botones[0].btn_trono; *&
  
 
 }
@@ -42,14 +42,14 @@ document.getElementById("btn_trono").innerHTML = obj_ENG.botones[0].btn_trono;
 if (LanguageON == "NAH"){
  
  // TITULO OBRA
-document.getElementById("titulo").innerHTML = obj_NAH.titulo; 
+//document.getElementById("titulo").innerHTML = obj_NAH.titulo; 
 
 // Textos Galerias
-document.getElementById("btn_lectura").innerHTML = obj_NAH.botones[0].btn_lectura; 
+/*document.getElementById("btn_lectura").innerHTML = obj_NAH.botones[0].btn_lectura; 
 document.getElementById("btn_atavios_sol").innerHTML = obj_NAH.botones[0].btn_atavios_sol;
 document.getElementById("btn_atavios_tierra").innerHTML = obj_NAH.botones[0].btn_atavios_tierra; 
 document.getElementById("btn_atavios_florido").innerHTML = obj_NAH.botones[0].btn_atavios_florido; 
-document.getElementById("btn_trono").innerHTML = obj_NAH.botones[0].btn_trono; 
+document.getElementById("btn_trono").innerHTML = obj_NAH.botones[0].btn_trono; */
 
 } // END NAH
 
