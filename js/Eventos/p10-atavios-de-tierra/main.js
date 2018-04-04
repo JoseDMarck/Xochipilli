3418ded90dd8f360008f03071a4cf31180633dc6
joseDeuteros <josewbmx@gmail.com>
2018-04-04 15:41:03 -0500

    Se agregó el contenido de atavios de sol

---
+++ js/Eventos/p10-atavios-de-tierra/main.js
@@ -0,0 +1,369 @@
+$( document ).ready(function() {
+    
+
+/* --------------------------------------------------------
+/* ---- Eventos interacción de pantalla
+-------------------------------------------------------- */
+
+HomeURL = '../../index.html';
+
+// IDIOMA DETECCIÓN
+LanguageON = window.location.hash.substring(1)
+
+
+
+if (LanguageON != "" ){
+    
+        if( LanguageON == "ESP" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_es_General").addClass("btn_idioma_es_on_General");
+    
+        } 
+        
+        if( LanguageON == "ENG" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_en_General").addClass("btn_idioma_en_on_General");
+            
+          
+        }
+    
+        if( LanguageON == "NAH" ){
+            console.log("Idioma: ", LanguageON);
+            $(".btn_idioma_na_General").addClass("btn_idioma_na_on_General");
+        } 
+    
+       
+    } else {
+       LanguageON = "ESP"; 
+       console.log("Idioma: Default");
+    }
+
+    var Change_idioma_ESP = function () {
+        location.reload();
+        window.location.href = '../../secciones/p10-atavios-de-tierra/index.html'+'#'+'ESP';
+    };
+    
+    
+    var Change_idioma_ENG = function () {
+        location.reload();
+        window.location.href = '../../secciones/p10-atavios-de-tierra/index.html'+'#'+'ENG';
+    };
+    
+    var Change_idioma_NAH = function () {
+        location.reload();
+        window.location.href = '../../secciones/p10-atavios-de-tierra/index.html'+'#'+'NAH';
+    };
+    
+
+    // EVENTOS SELECT IDIOMA ===================
+
+    $(".btn_idioma_es_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ESP });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_en_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_ENG });
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+    $(".btn_idioma_na_General").click(function(event) {
+        TweenMax.to("body", 1, {alpha:0, onComplete: Change_idioma_NAH});
+        LanguageON = null;
+        event.stopPropagation();
+    });
+
+
+var GotoObra = function () {
+    window.location.href = '../../secciones/p03-obra/index.html';
+};
+
+
+var GotoHome = function () {
+    window.location.href = '../../secciones/p02-home/index.html' +'#'+LanguageON;
+};
+
+var GotoBack = function () {
+    window.location.href = '../../secciones/p05-interpretacion/index.html' +'#'+LanguageON;;
+};
+
+
+
+var closem = function () {
+    TweenMax.to(".menu-General", 1, {right:"-11%", alpha:1 });
+};
+
+var opensem = function () {
+    TweenMax.to(".menu-General", 1, {right:"0%", alpha:1, onComplete: closem });
+    //$(".textos").fadeIn();
+};
+
+
+ 
+
+$(".btn_back").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoBack});
+});
+
+
+$(".btn_inicio_General").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoHome});
+});
+
+
+
+$(".btn_Obra").click(function() {
+    TweenMax.to("body", 1, {alpha:0, onComplete:  GotoObra });
+});
+
+
+
+var InitScreen = function () {
+    TweenMax.to("body", 3, {alpha:1});
+
+     // Para efecto de ayuda
+     TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:1920, height:1080, alpha: 1, x:0, y:0, rotation:360,  ease: Circ.easeOut, onComplete: 
+        close = function(){
+
+            CloseAyudaOnInitEvent = setTimeout(function(){ 
+                TweenMax.to(".ayuda-image", 0.8, {alpha: 1, width:0, height:0, alpha: 0, x:275, y: -20, rotation:180, ease: Circ.easeOut})
+            }, 5000);
+            
+        }   
+    })
+
+
+};
+
+
+InitScreen();
+
+
+// EVENTOS AYUDA ===================
+$(".btn_ayuda_General").click(function() {
+    TweenMax.to(".ayuda-image", 0.8, { alpha: 1, width:1920, height:1080, x:0, y:0, rotation:360, ease: Circ.easeOut});    
+});
+
+$(".ayuda-image").click(function() {
+    TweenMax.to(".ayuda-image", 0.8  , {alpha: 1, width:0, height:0, x:275, y: -20, rotation:180,   ease: Circ.easeOut});    
+    clearTimeout(CloseAyudaOnInitEvent); // Limpiamos el timer de 5 segundos
+});
+
+
+ 
+
+ 
+
+// EVENTOS GALLERIA HP ===================
+
+var num_total_HP = 4;
+var num_init_HP;
+var num_gal_HP;
+
+
+$(".btn_hotspots-LG").click(function() {
+
+    valor_hp = $(this).attr('class').split(' ')[1];
+    console.log(valor_hp);
+
+    if( valor_hp == "btn_L-1-1_HP" ){
+        num_gal_HP = 1;
+        num_init_HP = 1;
+    }
+
+    if( valor_hp == "btn_L-1-2_HP" ){
+        num_gal_HP = 1;
+        num_init_HP = 2;
+    }
+
+    if( valor_hp == "btn_L-1-3_HP" ){
+        num_gal_HP = 1;
+        num_init_HP = 3;
+    }
+
+    if( valor_hp == "btn_L-1-4_HP" ){
+        num_gal_HP = 1;
+        num_init_HP = 4;
+    }
+
+
+    if( valor_hp == "btn_L-2-1_HP" ){
+        num_gal_HP = 2;
+        num_init_HP = 1;
+    }
+
+    if( valor_hp == "btn_L-2-2_HP" ){
+        num_gal_HP = 2;
+        num_init_HP = 2;
+    }
+
+    if( valor_hp == "btn_L-2-3_HP" ){
+        num_gal_HP = 2;
+        num_init_HP = 3;
+    }
+
+    if( valor_hp == "btn_L-2-4_HP" ){
+        num_gal_HP = 2;
+        num_init_HP = 4;
+    }
+
+
+    if( valor_hp == "btn_L-3-1_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 1;
+    }
+
+    if( valor_hp == "btn_L-3-2_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 2;
+    }
+
+    if( valor_hp == "btn_L-3-3_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 3;
+    }
+
+    if( valor_hp == "btn_L-3-4_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 4;
+    }
+
+
+    if( valor_hp == "btn_L-3-1_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 1;
+    }
+
+    if( valor_hp == "btn_L-3-2_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 2;
+    }
+
+    if( valor_hp == "btn_L-3-3_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 3;
+    }
+
+    if( valor_hp == "btn_L-3-4_HP" ){
+        num_gal_HP = 3;
+        num_init_HP = 4;
+    }
+
+    if( valor_hp == "btn_L-4-1_HP" ){
+        num_gal_HP = 4;
+        num_init_HP = 1;
+    }
+
+    if( valor_hp == "btn_L-4-2_HP" ){
+        num_gal_HP = 4;
+        num_init_HP = 2;
+    }
+
+    if( valor_hp == "btn_L-4-3_HP" ){
+        num_gal_HP = 4;
+        num_init_HP = 3;
+    }
+
+    if( valor_hp == "btn_L-4-4_HP" ){
+        num_gal_HP = 4;
+        num_init_HP = 4;
+    }
+
+
+
+
+    // para mostrar el btn de prev
+    if (num_init_HP < 2 ){
+        $(".btn_prev_HP").hide();
+    }
+    else{
+        $(".btn_prev_HP").show();
+    }
+
+    // para mostrar el btn de next
+    if (num_init_HP < 4  ){
+        $(".btn_next_HP").show();
+    }
+    else{
+         $(".btn_next_HP").hide();
+    }
+
+    $(".PopUpSlider_HP").fadeIn();
+    $(".imagen-AT-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+    $(".desc-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+
+});
+
+
+
+$(".btn_next_HP").click(function() {
+     if (num_init_HP == 3){
+        $(".btn_next_HP").hide();
+     }
+
+    if (num_init_HP > 0){
+        $(".btn_prev_HP").show();
+     }
+
+
+    if (num_init_HP < num_total_HP){
+        $(".gal_img").hide();
+        $(".gal_desc_LG").hide();
+
+        num_init_HP = num_init_HP + 1;
+        console.log("num_init_HP", num_init_HP);
+        $(".imagen-AT-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+        $(".desc-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+
+         
+    }
+});
+
+
+$(".btn_prev_HP").click(function() {
+
+    if (num_init_HP == 2){
+        $(".btn_prev_HP").hide();
+     }
+
+    if (num_init_HP > 1){
+        $(".gal_img").hide();
+        $(".gal_desc_LG").hide();
+
+        num_init_HP = num_init_HP - 1;
+        console.log("num_init_HP", num_init_HP);
+        $(".imagen-AT-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+        $(".desc-"+num_gal_HP+"-"+num_init_HP).fadeIn();
+
+        $(".btn_next_HP").show();
+    }
+     
+
+});
+
+
+$(".btn_close_HP").click(function() {
+    $(".PopUpSlider_HP").fadeOut();
+    $(".gal_img").hide();
+    $(".gal_desc_LG").hide();
+    num_init_HP = 1;
+    
+});
+
+
+// =============== FUNCIONALIDAD PARA EL ZOOM
+
+$(function () {
+    var viewer = ImageViewer({zoomValue:160});
+    $('.full_btn').click(function () {
+        var imgSrc = "../../images/p04-representaciones/full_img"+num_init+".jpg",
+        highResolutionImage = $(this).data('high-res-img');
+        viewer.show(imgSrc, highResolutionImage);
+    });
+}); 
+
+
+
+}); // =============== onDocumetReady
