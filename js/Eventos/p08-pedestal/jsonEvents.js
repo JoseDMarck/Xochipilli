42008cd2758cc38466db4ec66103c4ea261a9e23
joseDeuteros <josewbmx@gmail.com>
2018-04-04 09:38:54 -0500

    Se agregó la sección de pedestal

---
+++ js/Eventos/p08-pedestal/jsonEvents.js
@@ -0,0 +1,127 @@
+$( document ).ready(function() {
+   
+  
+if (LanguageON == "ESP"){
+
+
+// TITULO OBRA
+//document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_ESP.descripcion; 
+
+// Titulos para los botones
+/*document.getElementById("titulo_btn_1").innerHTML = obj_ESP.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_ESP.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_ESP.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_ESP.titulos_btns[0].figura_4; */
+
+// Para las descripciones
+/*document.getElementById("gal_desc-1-1").innerHTML = obj_ESP.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_ESP.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_ESP.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_ESP.desc_gal[0].descripcion_1_4; 
+
+document.getElementById("gal_desc-2-1").innerHTML = obj_ESP.desc_gal[0].descripcion_2_1; 
+document.getElementById("gal_desc-2-2").innerHTML = obj_ESP.desc_gal[0].descripcion_2_2; 
+document.getElementById("gal_desc-2-3").innerHTML = obj_ESP.desc_gal[0].descripcion_2_3; 
+document.getElementById("gal_desc-2-4").innerHTML = obj_ESP.desc_gal[0].descripcion_2_4; 
+
+document.getElementById("gal_desc-3-1").innerHTML = obj_ESP.desc_gal[0].descripcion_3_1; 
+document.getElementById("gal_desc-3-2").innerHTML = obj_ESP.desc_gal[0].descripcion_3_2; 
+document.getElementById("gal_desc-3-3").innerHTML = obj_ESP.desc_gal[0].descripcion_3_3; 
+document.getElementById("gal_desc-3-4").innerHTML = obj_ESP.desc_gal[0].descripcion_3_4; 
+
+document.getElementById("gal_desc-4-1").innerHTML = obj_ESP.desc_gal[0].descripcion_4_1; 
+document.getElementById("gal_desc-4-2").innerHTML = obj_ESP.desc_gal[0].descripcion_4_2; 
+document.getElementById("gal_desc-4-3").innerHTML = obj_ESP.desc_gal[0].descripcion_4_3; 
+document.getElementById("gal_desc-4-4").innerHTML = obj_ESP.desc_gal[0].descripcion_4_4; */
+
+ 
+}
+
+
+ 
+if (LanguageON == "ENG"){
+
+// TITULO OBRA
+//document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_ENG.descripcion; 
+
+// Titulos para los botones
+/*document.getElementById("titulo_btn_1").innerHTML = obj_ENG.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_ENG.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_ENG.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_ENG.titulos_btns[0].figura_4; */
+
+// Para las descripciones
+/*document.getElementById("gal_desc-1-1").innerHTML = obj_ENG.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_ENG.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_ENG.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_ENG.desc_gal[0].descripcion_1_4; 
+
+document.getElementById("gal_desc-2-1").innerHTML = obj_ENG.desc_gal[0].descripcion_2_1; 
+document.getElementById("gal_desc-2-2").innerHTML = obj_ENG.desc_gal[0].descripcion_2_2; 
+document.getElementById("gal_desc-2-3").innerHTML = obj_ENG.desc_gal[0].descripcion_2_3; 
+document.getElementById("gal_desc-2-4").innerHTML = obj_ENG.desc_gal[0].descripcion_2_4; 
+
+document.getElementById("gal_desc-3-1").innerHTML = obj_ENG.desc_gal[0].descripcion_3_1; 
+document.getElementById("gal_desc-3-2").innerHTML = obj_ENG.desc_gal[0].descripcion_3_2; 
+document.getElementById("gal_desc-3-3").innerHTML = obj_ENG.desc_gal[0].descripcion_3_3; 
+document.getElementById("gal_desc-3-4").innerHTML = obj_ENG.desc_gal[0].descripcion_3_4; 
+
+document.getElementById("gal_desc-4-1").innerHTML = obj_ENG.desc_gal[0].descripcion_4_1; 
+document.getElementById("gal_desc-4-2").innerHTML = obj_ENG.desc_gal[0].descripcion_4_2; 
+document.getElementById("gal_desc-4-3").innerHTML = obj_ENG.desc_gal[0].descripcion_4_3; 
+document.getElementById("gal_desc-4-4").innerHTML = obj_ENG.desc_gal[0].descripcion_4_4;  */
+
+
+}
+
+if (LanguageON == "NAH"){
+
+    // TITULO OBRA
+//document.getElementById("titulo").innerHTML = obj_NAH.titulo; 
+
+// DESCRIPCIÓN OBRA
+document.getElementById("descripcion").innerHTML = obj_NAH.descripcion; 
+
+// Titulos para los botones
+/*document.getElementById("titulo_btn_1").innerHTML = obj_NAH.titulos_btns[0].figura_1; 
+document.getElementById("titulo_btn_2").innerHTML = obj_NAH.titulos_btns[0].figura_2; 
+document.getElementById("titulo_btn_3").innerHTML = obj_NAH.titulos_btns[0].figura_3; 
+document.getElementById("titulo_btn_4").innerHTML = obj_NAH.titulos_btns[0].figura_4; */
+
+
+// Para las descripciones
+/*document.getElementById("gal_desc-1-1").innerHTML = obj_NAH.desc_gal[0].descripcion_1_1; 
+document.getElementById("gal_desc-1-2").innerHTML = obj_NAH.desc_gal[0].descripcion_1_2; 
+document.getElementById("gal_desc-1-3").innerHTML = obj_NAH.desc_gal[0].descripcion_1_3; 
+document.getElementById("gal_desc-1-4").innerHTML = obj_NAH.desc_gal[0].descripcion_1_4; 
+
+document.getElementById("gal_desc-2-1").innerHTML = obj_NAH.desc_gal[0].descripcion_2_1; 
+document.getElementById("gal_desc-2-2").innerHTML = obj_NAH.desc_gal[0].descripcion_2_2; 
+document.getElementById("gal_desc-2-3").innerHTML = obj_NAH.desc_gal[0].descripcion_2_3; 
+document.getElementById("gal_desc-2-4").innerHTML = obj_NAH.desc_gal[0].descripcion_2_4; 
+
+document.getElementById("gal_desc-3-1").innerHTML = obj_NAH.desc_gal[0].descripcion_3_1; 
+document.getElementById("gal_desc-3-2").innerHTML = obj_NAH.desc_gal[0].descripcion_3_2; 
+document.getElementById("gal_desc-3-3").innerHTML = obj_NAH.desc_gal[0].descripcion_3_3; 
+document.getElementById("gal_desc-3-4").innerHTML = obj_NAH.desc_gal[0].descripcion_3_4; 
+
+document.getElementById("gal_desc-4-1").innerHTML = obj_NAH.desc_gal[0].descripcion_4_1; 
+document.getElementById("gal_desc-4-2").innerHTML = obj_NAH.desc_gal[0].descripcion_4_2; 
+document.getElementById("gal_desc-4-3").innerHTML = obj_NAH.desc_gal[0].descripcion_4_3; 
+document.getElementById("gal_desc-4-4").innerHTML = obj_NAH.desc_gal[0].descripcion_4_4;*/ 
+ 
+ 
+
+
+} // END NAH
+
+
+TweenMax.to(".btn_hotspots-LG", 2, {alpha:0.2,  repeat:-1, yoyo:true});
+
+}); // =============== onDocumetReady
