2b2485ea6271937bf08488107c4337ec784cf8f4
Lupe <guadalupe.butandave@gmail.com>
2018-04-11 16:20:57 -0500

    Gabinete botanico - Descripciones en ES, EN, NA de la sección de Interpretación

--- json/p09-atavios-sol/p09-atavios-sol.js
+++ json/p09-atavios-sol/p09-atavios-sol.js
@@ -2,7 +2,7 @@
 
 var obj_ESP = {
     titulo: "Bañado de sol",
-    descripcion: "<p> </p>",
+    descripcion: "<p>Xochipilli ostenta por doquier los signos del sol. Un sutil rastro de rojo, color de los dioses solares, cubre la piedra entera como un resplandor. Su cabeza se corona con un manto rico, orlado de plumas de ave roja, diurna y preciosa como el <i>tlauhquechol</i>.</p><p>El calor del sol, como el del amanecer, fuente de vida que fecunda la tierra, irradia y se posa en Xochipilli en forma de <i>tonalli</i>, conjuntos de cuatro círculos que se sitúan en el tocado de su cabeza y junto a las flores de su asiento.</p><p>En su tocado aparece también el <i>tlapapalli</i>, cuatro líneas “de varios colores”, que junto al <i>tonalli</i> evoca la exuberancia y lozanía propias de la fertilidad plena. El aspecto solar de Xochipilli se fusiona con los atributos que le fueron heredados de la tierra.</p>",
     titulos_btns: 
         [{
             figura_1: "Figura 1",
@@ -42,8 +42,8 @@ var obj_ESP = {
 
 
 var obj_ENG = {
-    titulo: "Sun Attire",
-    descripcion: "<p> </p>",
+    titulo: "Bathed by the Sun",
+    descripcion: "<p>Sun signs appear all over Xochipilli’s body. A subtle trace of red, the color of sun gods, blankets the entire sculpture as if in radiance. His head is crowned by a lavish cloth edged with feathers from a precious red bird associated with daytime such as the <i>tlauhquechol</i>, the roseate spoonbill.</p><p>The sun’s warmth, as at dawn, the source of life fertilizing the earth, shines and falls on Xochipilli in the form of <i>tonalli</i>, clusters of four circles on his headdress and beside the flowers on his seat.</p><p>His headdress bears the <i>tlapapalli</i>, four lines “of various colors,” which together with the <i>tonalli</i> evoke the exuberance and vitality of fertility in all its abundance. Xochipilli’s solar dimension merges with the attributes given to him by the earth.</p>",
     titulos_btns: 
         [{
             figura_1: "Figure 1",
@@ -82,8 +82,8 @@ var obj_ENG = {
 // IDIOMA NAH
 
 var obj_NAH = {
-    titulo: "Akatonali",
-    descripcion: "<p> </p>",
+    titulo: "Motonaltenqui",
+    descripcion: "<p>Incampa tetlaittitia imachiyo in tonatiuh in Xochipilli. In tetl quitlatilia in chichiltic imatiliztli in intlapal teteo, quitlatilia iuhqui ca yehuatl in tlaztallotl. Oquitlatocayoti in neolololli itzontecomauh. Mahuichichihulo in neolololli ica in chichiltic ihuitl iuhqui in <i>tlauhquechol</i>.</p><p>In itonal in tonatiuh neneuhca itonal in tlaztallotl in tenemitia in tlalchihua. In <i>tonalli</i>, motlalia Xochipilco. Motlali nahui yahualtic machiyotl ipan itzontecoma in Xochipilli totloc in inyeyan in xochime.</p><p>Itla ipan moquixtia in <i>tlapapalli</i> ipan itlamachiyotlalil in itzontecoma. Ca in tlapapalli in nahui tlapalcempantli in totloc in <i>tonalli</i> tetlaittitia in pactinemiliztli ihuan in miec tlatquitl in tlacachihua in yehuantin. In itonalixconexiliz in Xochipilli quicentetilia in tlamantli in oquimacac in tlalli.</p>",
     
     titulos_btns: 
         [{
--- json/p11-atavio-florido/p11-atavio-florido.js
+++ json/p11-atavio-florido/p11-atavio-florido.js
@@ -2,7 +2,7 @@
 
 var obj_ESP = {
     titulo: "Señor de todas las flores",
-    descripcion: "<p> </p>",
+    descripcion: "<p>Xochipilli porta sobre su piel la vestidura más exquisita de todas: flores preciosas lo visten y lo invisten de deidad. Es Señor de todas ellas en su entera diversidad y su belleza, símbolos de la fertilidad en su expresión más plena.</p><p>Sus flores coronan y engalanan. Son signos de canto, divinidad o poder; de sensualidad, guerra o muerte. Son brotes fragantes que endulzan; otros conducen al trance que da voz a los dioses. Todas encierran significados infinitos y algunas, en sólo cuatro pétalos, contienen el universo entero.</p> <p>Sobre el cuerpo de Xochipilli las flores son un alarde del ciclo vital que, como parte del cosmos mexica, nace a la vida por acción del calor del sol tras vadear la muerte en los húmedos y oscuros adentros de la tierra.</p>",
     titulos_btns: 
         [{
             figura_1: "Figura 1",
@@ -42,8 +42,8 @@ var obj_ESP = {
 
 
 var obj_ENG = {
-    titulo: "Sun Attire",
-    descripcion: "<p> </p>",
+    titulo: "Lord of All Flowers",
+    descripcion: "<p>Xochipilli wears on his skin the most exquisite garb of all: precious flowers adorning and granting him power as a deity. He is the lord of all flowers in all their diversity and beauty, symbols of the fullest expression of fertility.</p><p>His flowers are meant to crown and adorn. They are signs of song, divinity, or power; of sensuality, warfare, or death. They are fragrant buds that sweeten; others produce trance states to give voice to the gods. All of them hold infinite meanings, while some contain the entire universe in a mere four petals.</p><p>On Xochipilli’s body, flowers are an exuberant display of the cycle of life, integral to the Mexica cosmos. Life is born with the sun’s warmth after overcoming death in the moist darkness of the depths of the earth.</p>",
     titulos_btns: 
         [{
             figura_1: "Figure 1",
@@ -82,8 +82,8 @@ var obj_ENG = {
 // IDIOMA NAH
 
 var obj_NAH = {
-    titulo: "Akatonali",
-    descripcion: "<p> </p>",
+    titulo: "In intecu mochintin xochime",
+    descripcion: "<p>Ca ehuapan itlaquemi in Xochipilli in tlamahuizchichiuhtli. In patiyo xochime quiquentia in yehuatl in teotl. Ica nenepan cualli xochitl itecu in tlamantli. Inin machiyotl zan xochichihua.</p><p>Tlatocayotilo inic xochime. Ca yehuantin in machiyotl icuica, iteoyouh, ihuelitiliz, inececeltiliz, iyaoyouh ihuan miquiliztli. In yehuantin in celticatontli in cualli ahuiyaya in tzopeliaya. Occe temama in celticatontli tehuicpa icochiliz quimaca in teteo. Mochintin xochime tetlaittitia in amo pouhqui tlamantli. Occe yehuantin quipiya mochi cemanahuatl.</p><p>Ca in xochime in yoliliztli ipan inacayo in Xochipilli in tlacat ipan incemanauh in mexica ipampa in tonalli. In tonatiuh moquixtia itic miquiliztli in icuechahuac in itliltic in tlalli.</p>",
     titulos_btns: 
         [{
             figura_1: "Tlakuilomachiotl 1",
