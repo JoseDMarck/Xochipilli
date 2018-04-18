83be5f488e3a05d8151b67de0a781a0dfa8e268b
Lupe <guadalupe.butandave@gmail.com>
2018-04-18 17:51:00 -0500

    Screensaver - acomodo de imágenes para que no se corten

--- js/Eventos/p0-screenSaver/main_Tween.js
+++ js/Eventos/p0-screenSaver/main_Tween.js
@@ -5,15 +5,15 @@ var tl_scale = new TimelineLite();
     //tl_scale.to(".imagen_1", 0, {scale:1.6 })
     //tl_scale.to(".imagen_4", 0, {scale:1.6 })
 
-    tl_inicial.to(".imagen_1", 9, {scale:1.03, opacity:0.9})
-    .to(".imagen_1", 6, {opacity:0, scale:.9, tint:0x000000,  ease:Power1.easeInOut})
+    tl_inicial.to(".imagen_1", 9, {scale:1.1, opacity:0.9})
+    .to(".imagen_1", 6, {opacity:0, scale:1, tint:0x000000,  ease:Power1.easeInOut})
 
     
-    .to(".imagen_2", 6, {scale:1.03, opacity:1},  "-=3" )
-    .to(".imagen_2", 6, {opacity:0, scale:.9, tint:0x000000, ease:Power1.easeInOut})
+    .to(".imagen_2", 6, {scale:1.1, opacity:1},  "-=3" )
+    .to(".imagen_2", 6, {opacity:0, scale:1, tint:0x000000, ease:Power1.easeInOut})
     
-    .to(".imagen_3", 6, {scale:1.03, opacity:1},  "-=3" )
-    .to(".imagen_3", 4, {opacity:0.1, scale:.9, tint:0x000009, ease:Power1.easeInOut, onComplete:reiniciar})
+    .to(".imagen_3", 6, {scale:1.1, opacity:1},  "-=3" )
+    .to(".imagen_3", 4, {opacity:0.1, scale:1, tint:0x000009, ease:Power1.easeInOut, onComplete:reiniciar})
 
     function reiniciar(){
       tl_inicial.restart();
