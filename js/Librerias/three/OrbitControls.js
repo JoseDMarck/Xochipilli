e9f6fb6b9cca2640b86cf980f5244991e0793e43
joseDeuteros <josewbmx@gmail.com>
2018-04-21 11:07:25 -0500

    Que no se vea el interior de la pieza "hueca"

--- js/Librerias/three/OrbitControls.js
+++ js/Librerias/three/OrbitControls.js
@@ -26,7 +26,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.target = new THREE.Vector3();
 
 	// How far you can dolly in and out ( PerspectiveCamera only )
-	this.minDistance = 40;
+	this.minDistance = 60;
 	this.maxDistance = 100;
 
 	// How far you can zoom in and out ( OrthographicCamera only )
