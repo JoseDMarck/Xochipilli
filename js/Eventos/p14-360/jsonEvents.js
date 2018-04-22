0eced261f2fb1f0d76f834a34b4944f424ec8628
joseDeuteros <josewbmx@gmail.com>
2018-04-21 19:11:58 -0500

    Se agregó le botón de cerrar en los 3 idiomas

--- js/Eventos/p14-360/jsonEvents.js
+++ js/Eventos/p14-360/jsonEvents.js
@@ -2,24 +2,6 @@ $( document ).ready(function() {
    
   
 if (LanguageON == "ESP"){
-
-
-// TITULO OBRA
-//document.getElementById("titulo").innerHTML = obj_ESP.titulo; 
-
-// DESCRIPCIÓN OBRA
-document.getElementById("titulo").innerHTML = obj_gabinete_ESP.flores[id_flor].titulo; 
-document.getElementById("titulo_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].title; 
-document.getElementById("texto_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].txt; 
-
-document.getElementById("titulo_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].title; 
-document.getElementById("texto_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].txt; 
-
-document.getElementById("titulo_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].title; 
-document.getElementById("texto_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].txt; 
-
-
-
  
 }
 
@@ -27,43 +9,18 @@ document.getElementById("texto_box_3").innerHTML = obj_gabinete_ESP.flores[id_fl
  
 if (LanguageON == "ENG"){
 
-// TITULO OBRA
-//document.getElementById("titulo").innerHTML = obj_ENG.titulo; 
-
-// DESCRIPCIÓN OBRA
-
-
-document.getElementById("titulo").innerHTML = obj_gabinete_ESP.flores[id_flor].titulo; 
-document.getElementById("titulo_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].title; 
-document.getElementById("texto_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].txt; 
-
-document.getElementById("titulo_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].title; 
-document.getElementById("texto_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].txt; 
-
-document.getElementById("titulo_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].title; 
-document.getElementById("texto_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].txt; 
-
-
+ $(".btn_cerrar").addClass("btn_cerrar_ENG");
 }
 
 if (LanguageON == "NAH"){
 
-    document.getElementById("titulo").innerHTML = obj_gabinete_ESP.flores[id_flor].titulo; 
-    document.getElementById("titulo_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].title; 
-    document.getElementById("texto_box_1").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[0].txt; 
-    
-    document.getElementById("titulo_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].title; 
-    document.getElementById("texto_box_2").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[1].txt; 
-    
-    document.getElementById("titulo_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].title; 
-    document.getElementById("texto_box_3").innerHTML = obj_gabinete_ESP.flores[id_flor].descripcion[2].txt; 
+ $(".btn_cerrar").addClass("btn_cerrar_NAH");
  
- 
+  
 
 
 } // END NAH
 
 
-TweenMax.to(".btn_hotspots-LG", 2, {alpha:0.2,  repeat:-1, yoyo:true});
 
 }); // =============== onDocumetReady
--- secciones/p14-360/index.php
+++ secciones/p14-360/index.php
@@ -60,45 +60,6 @@
 	
 
 
-	<div class="PopUpSlider">
-		
-		
-				<div class="SeccionTitulo">
-					<div class="titulo_conservacion btn_titulo_Seccion seccion-m-1 "></div>
-					<div class="titulo_glosario btn_titulo_Seccion seccion-m-2"></div>
-					<div class="titulo_referencias btn_titulo_Seccion seccion-m-3"></div>
-					<div class="titulo_creditos btn_titulo_Seccion seccion-m-4"></div>
-				</div>
-		
-		
-				<div class="btn_close"></div>
-				<div class="btn_prev"></div>
-				<div class="btn_next"></div>
-		
-				<div class="Seccion-info">
-				
-					<div class="Textos seccion-m-1 flexcroll">
-							<div id="descripcion_conservacion" class="descripcion"></div>
-					</div>
-		
-		
-					<div class="Textos seccion-m-2 flexcroll">
-						<div id="descripcion_glosario" class="descripcion"></div>
-					</div>
-		
-					<div class="Textos seccion-m-3 flexcroll">
-						<div id="descripcion_referencias" class="descripcion"></div>
-					</div>
-		
-					<div class="Textos seccion-m-4 flexcroll">
-						<div id="descripcion_creditos" class="descripcion"></div>
-					</div>
-		
-					
-	
-				</div><!-- imagen_PopUpSlider -->
-		
-	</div><!-- PopUpSldier -->
 
 
 </div> <!--  main -->
@@ -114,6 +75,7 @@
 <script type="text/javascript" src="../../js/greensock-js/src/minified/TweenMax.min.js"></script>
 <script type="text/javascript" src="../../js/Librerias/imageviewer.js"></script>
 <script type="text/javascript" src="../../js/Eventos/p14-360/main.js"></script>
+<script type="text/javascript" src="../../js/Eventos/p14-360/jsonEvents.js"></script>
 <script type="text/javascript" src="../../js/Eventos/generales/screenSaver.js"></script>
 <script src="../../js/Eventos/p14-360/3DEvents.js"></script>
 
