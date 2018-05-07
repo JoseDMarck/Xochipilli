267e138bfbddb27b13b43a9d685e60c01d40f836
joseDeuteros <josewbmx@gmail.com>
2018-05-06 22:11:42 -0500

    adaptación de detalle de galería No 9

--- js/Eventos/p13-detalle-flor/main.js
+++ js/Eventos/p13-detalle-flor/main.js
@@ -397,6 +397,23 @@ if(id_flor == 8 ){
     pos_do_init = 2;
     pos_do_end = 11;
 
+
+    pos_xo_init = Number( urlParams["pos_xo_init"] );
+    pos_xo_start = 0;
+    num_init_G_xo = pos_xo_init;
+    pos_xo_end = 0;
+
+    pos_na_init = Number( urlParams["pos_na_init"] );
+    pos_na_start = 0;
+    num_init_G_na = pos_na_init;
+    pos_na_end = 1;
+
+    pos_do_init = Number( urlParams["pos_do_init"] );
+    pos_do_start = 2;
+    num_init_G_do = pos_do_init;
+    pos_do_end = 11;
+
+
     if(LanguageON == "ESP"){
         $(".ficha").addClass("ficha_9_ESP");
     }
